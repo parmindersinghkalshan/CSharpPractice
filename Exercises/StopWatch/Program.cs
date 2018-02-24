0964c1b990b605c0648ee6ab694e49a5e1d72c7b
Parminder Singh Kalshan <parmindersinghkalshan@gmail.com>
2018-02-24 18:26:54 -0500

    Exercise for creating a stop watch

---
+++ Exercises/StopWatch/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+
+            for (int i = 1; i <= 2; i++)
+            {
+                stopwatch.Start();
+                Thread.Sleep(4000);
+                stopwatch.Stop();
+                stopwatch.Duration();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exercises/StopWatch/Stopwatch.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace StopWatch
+{
+    public class Stopwatch
+    {
+        private bool watchAvailable = true;
+        private DateTime startTime;
+        private DateTime stopTime;
+        private TimeSpan duration;
+
+        public void Start()
+        {
+            if (!watchAvailable)
+            {
+                throw new InvalidOperationException("Can not start watch twice");
+            }
+
+            startTime = DateTime.Now;
+            watchAvailable = false;
+            Console.WriteLine($"Start Time = {startTime}");
+        }
+
+        public void Stop()
+        {
+            stopTime = DateTime.Now;
+            watchAvailable = true;
+            Console.WriteLine($"Stop Time = {stopTime}");
+        }
+
+        public void Duration()
+        {
+            duration = startTime - stopTime;
+            Console.WriteLine($"Duration = {(stopTime - startTime)}");
+        }
+    }
+}
