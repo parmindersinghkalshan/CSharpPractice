ebe7b97b52a858698286e8e226c0bcd6506bb33b
pskalshan <pskalshan@PCYULD0038>
2018-03-01 13:29:11 -0500

    Generics examples

---
+++ Exercises/Generics/InterfaceConstraint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class InterfaceConstraint<T> where T : IComparable
+    {
+        public T Max(T inta, T intb)
+        {
+            return inta.CompareTo(intb) > 0 ? inta : intb;
+        }
+    }
+}
---
+++ Exercises/Generics/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static void InterfaceComparer()
+        {
+            var interfaceConstraint = new InterfaceConstraint<int>();
+            interfaceConstraint.Max(10, 30);
+        }
+
+        static void ClassConstr()
+        {
+            var classConstraint = new ClassConstraint<Product>();
+            classConstraint.Display(new Product());
+        }
+
+        static void ValueType()
+        {
+            var valueType = new ValueTypeConstraint<int>();
+        }
+
+        static void ReferenceType()
+        {
+            var refType = new ReferenceTypeConstraint<string>();
+
+            var refType1 = new ReferenceTypeConstraint<Product>();
+        }
+
+        static void DefaultConstructor()
+        {
+            var defaultConstructor = new ObjectWithDefaultConstructor<Product>();
+        }
+    }
+}
