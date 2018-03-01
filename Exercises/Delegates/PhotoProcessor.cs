430405e4d2ad210b76d0947b917ddec1c0d54c7f
pskalshan <pskalshan@PCYULD0038>
2018-03-01 14:06:52 -0500

    DelegatesExample

---
+++ Exercises/Delegates/PhotoProcessor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public class PhotoProcessor
+    {
+        public delegate void PhotoFilterHandler(Photo photo);
+
+        public void Process(Photo photo, PhotoFilterHandler filterHandler)
+        {
+            filterHandler(photo);
+        }
+    }
+}
